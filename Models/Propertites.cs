c42e46794bf4030255bbc0d86fe1490646ad17db
fenghui.xu <fenghui.xu@ly.com>
2021-02-19 13:26:16 +0800

    rebuild property object.

--- Models/Propertites.cs
+++ Models/Propertites.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using Walle.Components.Extensions;
 
-namespace Walle.Components
+namespace Walle.Components.Models
 {
     public class Propertites : List<Propertity>
     {
@@ -74,43 +74,6 @@ namespace Walle.Components
         }
     }
 
-    public class Propertity
-    {
-        public Propertity()
-        {
-
-        }
-        public Propertity(string name, string value)
-        {
-            this.Name = name;
-            this.Value = value;
-        }
-
-        public Propertity(string name, string value, string desc)
-        {
-            this.Name = name;
-            this.Value = value;
-            this.Description = desc;
-        }
-
-        public static Propertity New(string name, string value)
-        {
-            return new Propertity(name, value);
-        }
-
-        public static Propertity New(string name, string value, string desc)
-        {
-            return new Propertity(name, value, desc);
-        }
-
-
-        public string Name { get; set; } = string.Empty;
-
-        public string Value { get; set; } = string.Empty;
-
-        public string Description { get; set; } = string.Empty;
-
-        public List<Propertity> SubPropertities { get; set; } = new List<Propertity>();
-    }
+   
 
 }
---
+++ Models/Propertity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Walle.Components.Models
+{
+    public class Propertity
+    {
+        public Propertity()
+        {
+
+        }
+        public Propertity(string name, string value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+
+        public Propertity(string name, string value, string desc)
+        {
+            this.Name = name;
+            this.Value = value;
+            this.Description = desc;
+        }
+
+        public static Propertity New(string name, string value)
+        {
+            return new Propertity(name, value);
+        }
+
+        public static Propertity New(string name, string value, string desc)
+        {
+            return new Propertity(name, value, desc);
+        }
+
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Value { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public List<Propertity> SubPropertities { get; set; } = new List<Propertity>();
+    }
+}
